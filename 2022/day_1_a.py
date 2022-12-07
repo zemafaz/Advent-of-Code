ce3030aad3abdda85afd1a30f14c6fb9adfa488d
José Azevedo <josemfazevedo.b@hotmail.com>
2022-12-07 19:07:52 +0000

    Solved 2022 Day 1 - Calorie Counting

---
+++ 2022/day_1_a.py
@@ -0,0 +1,16 @@
+
+f = open("./day_1_input", "r")
+max_calories = 0
+
+current = 0
+
+for line in f:
+    if line == "\n":
+        max_calories = max(max_calories, current)
+        current = 0
+    else:
+        current += int(line)
+
+print(max_calories)
+
+f.close()
